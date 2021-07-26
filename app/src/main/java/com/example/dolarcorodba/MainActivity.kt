696fe71039db6999cf28e409ff41ee853132a501
k-1-e <obregonkevin1351@gmail.com>
2021-07-25 18:07:34 -0600

    convertidor de dolares a cordobas

--- app/src/main/java/com/example/dolarcorodba/MainActivity.kt
+++ app/src/main/java/com/example/dolarcorodba/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.dolarcorodba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun Calcular(view: View) {
+
+        var Resultado : Double
+        var Tipocambio = findViewById<EditText>(R.id.edtcambio)
+        var cantidad = findViewById<EditText>(R.id.edtcantidad)
+        var resultado = findViewById<TextView>(R.id.edtresultado)
+       // var convertir = findViewById<Button>(R.id.btnconvertir)
+        var cordobasadolar = findViewById<RadioButton>(R.id.cordobasdolar)
+        var dolaresacordobas = findViewById<RadioButton>(R.id.dolarcordobas)
+
+
+
+        var opcion = findViewById<RadioButton>(R.id.cordobasdolar)
+
+        if (cordobasadolar.isChecked){
+            Resultado = ((cantidad.text.toString().toDouble())/(Tipocambio.text.toString().toDouble()))
+            resultado.setText(Resultado.toString())
+        } else if (dolaresacordobas.isChecked) {
+            Resultado = ((cantidad.text.toString().toDouble())*(Tipocambio.text.toString().toDouble()))
+            resultado.setText(Resultado.toString())
+        } else {
+            resultado.setText("Por favor, seleccione opción de moneda a convertir")
+        }
+
+
+    }
+
 }
